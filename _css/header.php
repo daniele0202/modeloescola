9f4c55d4204518cee9c130e6e9f2171d6090eec9
Daniele <59766706+daniele0202@users.noreply.github.com>
2023-01-25 10:09:56 -0300

    adicionar arquivos
    adicionar arquivos

---
+++ _css/header.php
@@ -0,0 +1,88 @@
+<head>
+    <link rel="stylesheet" href="./_css/header.css">
+    <link rel="stylesheet" href="./_css/stylo_index.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@500;600&display=swap&family=Satisfy&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lobster+Two&display=swap" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+ 
+</head>
+   
+    <section class="midiaca">
+    <div id="port"><a href="login.php" style='background-color: #DC143C; color: white;font-weight: 550; border-radius: 5px;'><img src="./images/portal.png" alt="" class="portal"></a></li>
+    </div>    
+
+
+<div class="midia1">
+        <a href="https://www.facebook.com/cebomjesusaju" target="_blank"><img src="images/facebr1.png" alt="" class="midia"></a>
+        <a href="https://www.instagram.com/centro_educacional_bom_jesus/" target="_blank"><img src="images/instbr.png" alt="" class="midia"></a>
+       <a href="https://goo.gl/maps/xXw41aeQYVCXj2fT6" target="_blank"><img src="images/loc01.png" alt="" class="midia"></a>
+       <a href="daniele:dani.jesus1080@gmail.com" target="_blank"><img src="images/gmailbranco.png" alt="" class="midia"></a>
+       <a href="" target="_blank"><img src="images/zapbr.png" alt="" class="midia"></a>
+       </div>
+    </section>
+
+    
+   
+    
+
+<header>
+
+<div id="sub-menu">
+        <img src="images/colegialb.png" alt="" id="logo"> 
+        <!-- <img src="./images/nomelog.png" alt="" id="logoi">  -->
+       <!-- <a href="#" id="nome">Bom Jesus</a> -->
+    </div>
+    
+
+
+    <!-- <div id="logo">
+        <img src="images/logocerto.png" alt="" >
+    </div>   -->
+
+        <!---->
+
+        <input type="checkbox" id="bt_menu">
+        <label for="bt_menu">&#9776;</label>
+ 
+            <nav class="menu">
+             <ul>
+             <li><a href="./index.php">Inicio</a></li>
+             <li><a href="#curso">Cursos</a></li>
+             <li><a href="quemsomos.php">Quem somos</a></li>
+             
+             <li><a href="quemsomos.php"> Documentos &#9776;</a>
+            
+             <ul>
+             <li><a href="index.php">Lista material</a></li>
+             <li><a href="#">Documento</a></li>
+             <li><a href="index.php">Livros</a></li>
+            </ul>
+            </li>
+
+            
+            <li><a href="#">Contatos</a></li>
+            
+            </ul>
+            </nav>
+        </div>
+
+</header> 
+
+<!-- <div id="separador"></div> -->
+
---
+++ _css/menuscript.js
@@ -0,0 +1,11 @@
+
+
+var menu = document.querySelector('nav ul');
+var menuIcon = document.querySelector('.menu-icon');
+
+function abrirMenu() {
+	
+	menuIcon.classList.toggle("open");
+	  
+}
+
